e8c74b324a036c516a68d786b788018d7438eaee
Oh-Donggeon <donggeon.dev@gmail.com>
2020-08-01 16:25:39 +0900

    initial commit

---
+++ SwiftPractice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPractice
+//
+//  Created by Oh Donggeon on 2020/08/01.
+//  Copyright © 2020 S033_DonggeonOh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
